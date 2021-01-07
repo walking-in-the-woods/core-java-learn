0de4944e92855fee2d3b2840838414476bdbb3a8
Alex S <as202009261140@gmail.com>
2021-01-07 12:10:00 +0300

    Immutable Class

--- CJLCollections/src/AdventureGame/AdventureGameMain.java
+++ CJLCollections/src/AdventureGame/AdventureGameMain.java
@@ -12,33 +12,39 @@ public class AdventureGameMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
         locations.put(0, new Location(0,
-                "You are sitting in front of a computer learning Java"));
+                "You are sitting in front of a computer learning Java", null));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
         locations.put(1, new Location(1,
-                "You are standing at the end of a road before a small brick building"));
+                "You are standing at the end of a road before a small brick building", tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 5);
         locations.put(2, new Location(2,
-                "You are at the top of a hill"));
+                "You are at the top of a hill", tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 1);
         locations.put(3, new Location(3,
-                "You are inside a building, a well house for a small spring"));
+                "You are inside a building, a well house for a small spring", tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
         locations.put(4, new Location(4,
-                "You are in a valley beside a stream"));
+                "You are in a valley beside a stream", tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
         locations.put(5, new Location(5,
-                "You are in the forest"));
-
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
-
-        locations.get(2).addExit("N", 5);
-
-        locations.get(3).addExit("W", 1);
-
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
+                "You are in the forest", tempExit));
 
         Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT", "Q");
@@ -50,6 +56,8 @@ public class AdventureGameMain {
         int loc = 1;
         while(true) {
             System.out.println(locations.get(loc).getDescription());
+            tempExit.remove("S");
+
             if(loc == 0) {
                 break;
             }
--- CJLCollections/src/AdventureGame/Location.java
+++ CJLCollections/src/AdventureGame/Location.java
@@ -1,5 +1,7 @@
 package AdventureGame;
 
+// https://docs.oracle.com/javase/tutorial/essential/concurrency/imstrat.html
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,16 +10,22 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>();
+        if(exits != null) {
+            this.exits = new HashMap<String, Integer>(exits);
+        } else {
+            this.exits = new HashMap<String, Integer>();
+        }
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
+
+
 
     public int getLocationID() {
         return locationID;
