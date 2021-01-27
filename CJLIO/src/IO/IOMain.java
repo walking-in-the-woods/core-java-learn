bd8277192c3e096476532f980e878c7081b12521
Alex S <as202009261140@gmail.com>
2021-01-27 16:29:25 +0300

    IO. Writing a file

--- CJLIO/src/IO/IOMain.java
+++ CJLIO/src/IO/IOMain.java
@@ -1,6 +1,16 @@
 package IO;
 
-// stopped at the video 5. Introduction to IO at 00:15:56
+/*
+java.nio package
+
+We should use character data (text format) to work with text reports, XML and JSON data.
+We should use byte data (binary format) to work with program variables or classes
+
+Sequential (serial) access can be thought as a stream of data that arrives at the program our is sent out from it.
+Random access only really applies to files and allows us to jump about the file or within the file retrieving
+or overriding any data in any location is more like using a database.
+
+*/
 
 public class IOMain {
     public static void main(String[] args) {
