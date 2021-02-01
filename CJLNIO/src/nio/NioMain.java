2e6dc43a712ff3546d98aa6d0d2954eb57a956d8
Alex S <as202009261140@gmail.com>
2021-02-01 16:07:46 +0300

    NIO. Chained put method

--- CJLNIO/src/nio/NioMain.java
+++ CJLNIO/src/nio/NioMain.java
@@ -49,6 +49,26 @@ a parameter we passed to the RandomAccessFile constructor ("r", "rw" .. ).
 
 // When something goes wrong with nio, flip.
 
+/*
+   read(ByteBuffer) -  reads bytes beginning at the channel's current position, and after the read,
+                       updates the position accordingly.
+                       Note that now we're talking about the channel's position, not the byte buffer's position.
+                       Of course, the bytes will be placed into the buffer starting at its current position.
+
+   write(ByteBuffer) - the same as read, except it writes. There's one exception.
+                       If a datasource is opened in APPEND mode, then the first write will take place starting
+                       at the end of the datasource, rather than at position 0. After the write, the position
+                       will be updated accordingly.
+
+   position() -        returns the channel's position.
+
+   position(long) -    sets the channel's position to the passed value.
+
+   truncate(long) -    truncates the size of the attached datasource to the passed value.
+
+   size() -            returns the size of the attached datasource
+*/
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -63,91 +83,44 @@ public class NioMain {
             FileChannel binChannel = binFile.getChannel()) {
 
             ByteBuffer buffer = ByteBuffer.allocate(100);
+
+            // Chained put method
+//            byte[] outputBytes = "Hello World!".getBytes();
+//            byte[] outputBytes2 = "Nice to meet you".getBytes();
+//            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
+//            buffer.flip();
+
+            // Unchained put method
             byte[] outputBytes = "Hello World!".getBytes();
             buffer.put(outputBytes);
+            long int1Pos = outputBytes.length;
             buffer.putInt(245);
+            long int2Pos = int1Pos + Integer.BYTES;
             buffer.putInt(-98765);
             byte[] outputBytes2 = "Nice to meet you".getBytes();
             buffer.put(outputBytes2);
+            long int3Pos = int2Pos + Integer.BYTES + outputBytes2.length;
             buffer.putInt(1000);
             buffer.flip();
+
             binChannel.write(buffer);
 
-//            //ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
-//            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
-//            buffer.put(outputBytes);
-//
-//            buffer.flip();
-//            int numBytes = binChannel.write(buffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-//            intBuffer.putInt(245);
-//            intBuffer.flip(); // to reset the position to zero
-//            numBytes = binChannel.write(intBuffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            intBuffer.flip();
-//            intBuffer.putInt(-98765);
-//            intBuffer.flip();
-//            numBytes = binChannel.write(intBuffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            RandomAccessFile ra = new RandomAccessFile("CJLNIO/data.dat", "rwd");
-//            FileChannel channel = ra.getChannel();
-//            outputBytes[0] = 'a';
-//            outputBytes[1] = 'b';
-//            buffer.flip(); // if we comment this flip(), we'll get "abllo World!" in output
-//            long numBytesRead = channel.read(buffer);
-//            if(buffer.hasArray()) {
-//                System.out.println("byte buffer = " + new String(buffer.array()));
-////                System.out.println("byte buffer = " + new String(outputBytes));
-//            }
-//
-//            // Absolute read
-//            intBuffer.flip();
-//            numBytesRead = channel.read(intBuffer);
-//            System.out.println(intBuffer.getInt(0));
-//            intBuffer.flip();
-//            numBytesRead = channel.read(intBuffer);
-//            intBuffer.flip(); // if we put this flip() here, next we do the relative read
-//            System.out.println(intBuffer.getInt(0));
-//            System.out.println(intBuffer.getInt());
-//
-//            // Relative read
-////            intBuffer.flip();
-////            numBytesRead = channel.read(intBuffer);
-////            intBuffer.flip();
-////            System.out.println(intBuffer.getInt());
-////            intBuffer.flip();
-////            numBytesRead = channel.read(intBuffer);
-////            intBuffer.flip();
-////            System.out.println(intBuffer.getInt());
-//
-//            channel.close();
-//            ra.close();
 
-//            System.out.println("outputBytes = " + new String(outputBytes));
+            RandomAccessFile ra = new RandomAccessFile("CJLNIO/data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
 
-//            RandomAccessFile ra = new RandomAccessFile("CJLNIO/data.dat", "rwd");
-//            byte[] b = new byte[outputBytes.length];
-//            ra.read(b);
-//            System.out.println(new String(b));
-//
-//            long int1 = ra.readInt();
-//            long int2 = ra.readInt();
-//            System.out.println(int1);
-//            System.out.println(int2);
-
-//            FileInputStream file = new FileInputStream("CJLNIO/data.txt");
-//            FileChannel channel = file.getChannel();
-
-//            Path dataPath = FileSystems.getDefault().getPath("CJLNIO/data.txt");
-//            Files.write(dataPath, "\nLine 4".getBytes("UTF-8"), StandardOpenOption.APPEND);
-//            List<String> lines = Files.readAllLines(dataPath);
-//            for(String line : lines) {
-//                System.out.println(line);
-//            }
+            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            byte[] inputString = new byte[outputBytes.length];
+            readBuffer.get(inputString);
+            System.out.println("inputString = " + new String(inputString));
+            System.out.println("int1 = " + readBuffer.getInt());
+            System.out.println("int2 = " + readBuffer.getInt());
+            byte[] inputString2 = new byte[outputBytes2.length];
+            readBuffer.get(inputString2);
+            System.out.println("inputString2 = " + new String(inputString2));
+            System.out.println("int3 = " + readBuffer.getInt());
 
         } catch (IOException e) {
             e.printStackTrace();
