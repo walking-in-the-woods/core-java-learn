a595a42ef6af8786da8f11a027480a433bca07a4
Alex S <as202009261140@gmail.com>
2021-02-02 09:58:15 +0300

    NIO. More on Paths

--- CJLNIO/src/paths/PathsMain.java
+++ CJLNIO/src/paths/PathsMain.java
@@ -28,6 +28,29 @@ Absolute path:
         Path dataPath = Paths.get("C:\\MyIdeaProjects\\Project1\\data.txt");
 */
 
+/*
+java.io.File class has some problems:
+
+1. Many methods in the class don't throw exceptions, or don't provide specific error messages when they fail.
+   One example would be the File.delete() method, which returns a boolean. If the deletion fails, you can't tell
+   if it was because the file didn't exist, or the application didn't have permission to delete the file, or .. ?
+   Unfortunately, the method doesn't provide that information.
+
+2. The File.rename() method works differently on different platforms. Java is supposed to be portable across platforms,
+   meaning the code shouldn't have to wirry about which operating system it's running on.
+
+3. There's no support for symbolic links. A symbolic link is a kind of file that points to another file. They're often
+   used with networks, to point to a remote location. The File class doesn't understand them.
+
+4. The File class doesn't provide a way to get metadata about a file, such as its permissions, its owner, and the
+   other security information. The metadata it does provide is retrieved inefficiently.
+
+5. Many of the methods don't perform well when working with lots of data. For example, if we use the File class
+   to request a list of all the files in a directory, and there are a lot of files, the method can hang.
+
+6. Since the File class doesn't understand symbolic links, walking a directory tree is problematic.
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.FileSystems;
@@ -40,11 +63,24 @@ public class PathsMain {
         Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
         printFile(path);
 
-        Path filePath = FileSystems.getDefault().getPath("CJLNIO/files", "SubdirectoryFile.txt");
+//        Path filePath = FileSystems.getDefault().getPath("CJLNIO/files", "SubdirectoryFile.txt");
+        Path filePath = Paths.get(".","CJLNIO/files", "SubdirectoryFile.txt");
         printFile(filePath);
 
-        filePath = Paths.get("D:\\code\\java\\JavaTimBuchalka\\OutThere.txt");
+//        filePath = Paths.get("D:\\code\\java\\JavaTimBuchalka\\OutThere.txt");
+        filePath = Paths.get("D:\\", "code\\java\\JavaTimBuchalka\\", "OutThere.txt");
         printFile(filePath);
+
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
+
+        // D:\Examples\.\subfolder\..\directory
+        // D:\Examples\directory           // .. means the parent directory (move up th the previous node)
+
+        Path path2 = FileSystems.getDefault().getPath(
+                ".", "CJLNIO/files", "..", "files", "SubdirectoryFile.txt");
+        System.out.println(path2.normalize().toAbsolutePath());
+        printFile(path2.normalize());
     }
 
     private static void printFile(Path path) {
