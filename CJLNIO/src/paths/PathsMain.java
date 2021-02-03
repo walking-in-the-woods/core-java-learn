3e2c021140e371a10b256ede393327c0c8ea325d
Alex S <as202009261140@gmail.com>
2021-02-04 00:46:10 +0300

    NIO. Mapping IO and NIO Methods. The end of this section

--- CJLNIO/src/paths/PathsMain.java
+++ CJLNIO/src/paths/PathsMain.java
@@ -112,6 +112,11 @@ the directory in the postVisitDirectory() method because we have to delete all t
 and all its descendants BEFORE we can delete the directory.
 */
 
+/*
+Use java.nio when working with a file system and when reading and writing file contents.
+Often, the java.io streams are the better choice.
+*/
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -179,6 +184,37 @@ public class PathsMain {
             Files.walkFileTree(dir2Path, new CopyFiles(dir2Path, copyPath));
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }
+
+        File file = new File("CJLNIO/files/SubdirectoryFile.txt");
+        Path convertedPath = file.toPath();
+        System.out.println("convertedPath = " + convertedPath);
+
+        File parent = new File("CJLNIO/files");
+        File resolvedFile = new File(parent, "dir/file.txt");
+        System.out.println(resolvedFile.toPath());
+
+        resolvedFile = new File("CJLNIO/files", "dir/file.txt");
+        System.out.println(resolvedFile.toPath());
+
+        Path parentPath = Paths.get("CJLNIO/files");
+        Path childRelativePath = Paths.get("dir/file.txt");
+        System.out.println(parentPath.resolve(childRelativePath));
+
+        File workingDirectory = new File("").getAbsoluteFile();
+        System.out.println("Working directory = " + workingDirectory.getAbsolutePath());
+
+        System.out.println("--- print Dir1 contents using list() ---");
+        File dir2File = new File(workingDirectory, "CJLNIO/FileTree/Dir2");
+        String[] dir2Contents = dir2File.list();
+        for(int i=0; i<dir2Contents.length; i++) {
+            System.out.println("i = " + i + ": " + dir2Contents[i]);
+        }
+
+        System.out.println("--- print Dir1 contents using listFiles() ---");
+        File[] dir2Files = dir2File.listFiles();
+        for(int i=0; i<dir2Files.length; i++) {
+            System.out.println("i = " + i + ": " + dir2Files[i].getName());
         }
     }
 }
