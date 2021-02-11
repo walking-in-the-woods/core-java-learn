2a294b3d74e3bbd5dd803efd97df33b9a7f0d68b
Alex S <as202009261140@gmail.com>
2021-02-11 09:50:27 +0300

    Concurrency. Service

--- CJLConcurrency/src/uiapp/Controller.java
+++ CJLConcurrency/src/uiapp/Controller.java
@@ -12,9 +12,10 @@ Whatever the value property changes, the items property will update accordingly.
 update the list view by calling platform.runLater.
 */
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
+import javafx.concurrent.Service;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -22,47 +23,49 @@ import javafx.scene.control.ProgressBar;
 
 public class Controller {
 
-    private Task<ObservableList<String>> task;
     @FXML
     private ListView listView;
-
     @FXML
     private ProgressBar progressBar;
-
     @FXML
     private Label progressLabel;
+    @FXML
+    private Service<ObservableList<String>> service;
 
     public void initialize() {
-        task = new Task<ObservableList<String>>() {
+        service = new EmployeeService();
+
+        progressBar.progressProperty().bind(service.progressProperty());
+        progressLabel.textProperty().bind(service.messageProperty());
+        listView.itemsProperty().bind(service.valueProperty());
+
+        service.setOnRunning(new EventHandler<WorkerStateEvent>() {
             @Override
-            protected ObservableList<String> call() throws Exception {
-                String[] names = {"Tim Buchalka",
-                        "Bill Rogers",
-                        "Jack Jill",
-                        "Joan Andrews",
-                        "Mary Johnson",
-                        "Bob McDonald"};
-
-                ObservableList<String> employees =  FXCollections.observableArrayList();
-
-                for(int i = 0; i < names.length; i++) {
-                    employees.add(names[i]);
-                    updateMessage("Added " + names[i] + " to the list");
-                    updateProgress(i + 1, names.length);
-                    Thread.sleep(500);
-                }
-
-                return employees;
+            public void handle(WorkerStateEvent event) {
+                progressBar.setVisible(true);
+                progressLabel.setVisible(true);
             }
-        };
+        });
 
-        progressBar.progressProperty().bind(task.progressProperty());
-        progressLabel.textProperty().bind(task.messageProperty());
-        listView.itemsProperty().bind(task.valueProperty());
+        service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent event) {
+                progressBar.setVisible(false);
+                progressLabel.setVisible(false);
+            }
+        });
+
+        progressBar.setVisible(false);
+        progressLabel.setVisible(false);
     }
 
     @FXML
     public void buttonPressed() {
-        new Thread(task).start();
+        if(service.getState() == Service.State.SUCCEEDED) {
+            service.reset();
+            service.start();
+        } else if(service.getState() == Service.State.READY) {
+            service.start();
+        }
     }
 }
