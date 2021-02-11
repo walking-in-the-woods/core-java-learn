6705380a311e4ec6805df8f2fc869ee952ecda5a
Alex S <as202009261140@gmail.com>
2021-02-11 13:44:06 +0300

    Concurrency. Challenge 3 and 4

--- CJLConcurrency/src/challenge/BankAccount.java
+++ CJLConcurrency/src/challenge/BankAccount.java
@@ -1,31 +1,34 @@
 package challenge;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private double balance;
     private String accountNumber;
+    private Lock lock;
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = new ReentrantLock();
     }
 
-//    public synchronized void deposit(double amount) {
-//        balance += amount;
-//    }
-//
-//    public synchronized void withdraw(double amount) {
-//        balance -= amount;
-//    }
-
     public void deposit(double amount) {
-        synchronized (this) {
+        lock.lock();
+        try {
             balance += amount;
+        } finally {
+            lock.unlock();
         }
     }
 
     public void withdraw(double amount) {
-        synchronized (this) {
+        lock.lock();
+        try {
             balance -= amount;
+        } finally {
+            lock.unlock();
         }
     }
 
--- CJLConcurrency/src/challenge/ChallengeMain.java
+++ CJLConcurrency/src/challenge/ChallengeMain.java
@@ -4,20 +4,6 @@ public class ChallengeMain {
     public static void main(String[] args) {
         final BankAccount account = new BankAccount("12345-678", 1000.00);
 
-//        Thread trTread1 = new Thread() {
-//            public void run() {
-//                account.deposit(300.00);
-//                account.withdraw(50.00);
-//            }
-//        };
-//
-//        Thread trTread2 = new Thread() {
-//            public void run() {
-//                account.deposit(203.75);
-//                account.withdraw(100.00);
-//            }
-//        };
-
         Thread trTread1 = new Thread(new Runnable() {
             @Override
             public void run() {
