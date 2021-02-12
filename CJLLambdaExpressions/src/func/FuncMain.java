11764c76461614ef341e9d61d75489a006d981a6
Alex S <as202009261140@gmail.com>
2021-02-12 08:21:00 +0300

    Lambda Expressions. Functions

--- CJLLambdaExpressions/src/func/FuncMain.java
+++ CJLLambdaExpressions/src/func/FuncMain.java
@@ -1,11 +1,54 @@
 package func;
 
+/*
+We've got our last names working OK then if this was something we wanted to frequently do, we would go probably added
+a getLasName() method to the Employee class, but we'll pretend that we don't want to do it for some reason.
+However, let's pretend that there are several places in our application we need to get the last name of a single
+employee. We want to use a lambda expression that maps to one of the java.util.function interfaces.
+Now, non of the interfaces we looked at so far matches what we want to do. Consumer doesn't return a value,
+Supplier doesn't accept any parameters, and Predicate only return true or false. So we want to actually pass in a
+String and we then expect a String to be returned. And this sounds like a job for the function interface.
+Now the interface represents a function that takes one parameter and returns a value. The functional method that one
+that we use with lambdas is the apply() method.
+*/
+
+/*
+An example of using functions:
+
+Old code:
+
+    public interface ResizeImage {
+        public Image resizeImage(Image image);
+    }
+
+    public List<Image> runWhenDone() {
+
+        // pretend we have access to an ImageResizer instance
+        // (probably provided when a Runnable object was constructed),
+        // and to a list called images that has all the images fetched from the Internet
+
+        for (Image image : images) {
+            resizedImages.add(resizer.resizeImage(image));
+        }
+    }
+
+New code:
+
+    Function<Image, Image> resizer1 = (Image image) -> { resize using algorithm 1 }
+    Function<Image, Image> resizer2 = (Image image) -> { resize using algorithm 2 }
+
+    public List<Image> runWhenDone(Function<Image, Image> resizer) {
+        for (Image image : images) {
+            resizedImages.add(resizer.apply(image));
+        }
+    }
+*/
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.function.IntPredicate;
+import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class FuncMain {
     public static void main(String[] args) {
@@ -26,37 +69,29 @@ public class FuncMain {
         employees.add(red);
         employees.add(charming);
 
-        printEmployeesByAge(employees, "Employees over 30: ", employee -> employee.getAge() > 30);
-        printEmployeesByAge(employees, "Employees 30 and under: ", employee -> employee.getAge() <= 30);
-        printEmployeesByAge(employees, "Employees younger than 25:",
-                new Predicate<Employee>() {
-                    @Override
-                    public boolean test(Employee employee) {
-                        return employee.getAge() < 25;
-                    }
-                });
+        Function<Employee, String> getLastName = (Employee employee) -> {
+            return employee.getName().substring(employee.getName().indexOf(' ') + 1);
+        };
 
-        IntPredicate greaterThan15 = i -> i > 15;
-        IntPredicate lessThan100 = i -> i < 100;
+//        String lastName = getLastName.apply(employees.get(5));
+//        System.out.println(lastName);
 
-        System.out.println(greaterThan15.test(10));
-        int a = 20;
-        System.out.println(greaterThan15.test(a + 5));
+        Function<Employee, String> getFirstName = (Employee employee) -> {
+            return employee.getName().substring(0, employee.getName().indexOf(' '));
+        };
 
-        System.out.println(greaterThan15.and(lessThan100).test(50));
-        System.out.println(greaterThan15.and(lessThan100).test(15));
-
-        Random random = new Random();
-
-        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
-        for (int i=0; i<10; i++) {
-            System.out.println(randomSupplier.get());
+        Random random1 = new Random();
+        for(Employee employee : employees) {
+            if(random1.nextBoolean()) {
+                System.out.println(getAName(getFirstName, employee));
+            } else {
+                System.out.println(getAName(getLastName, employee));
+            }
         }
+    }
 
-        System.out.println("---");
-        for (int i=0; i<10; i++) {
-            System.out.println(random.nextInt(1000));
-        }
+    private static String getAName(Function<Employee, String> getName, Employee employee) {
+       return  getName.apply(employee);
     }
 
     private static void printEmployeesByAge(List<Employee> employees,
