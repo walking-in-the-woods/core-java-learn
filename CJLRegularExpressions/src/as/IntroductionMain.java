055e65c701cdc5680659b1c5b865baa44475dcc4
Alex S <as202009261140@gmail.com>
2021-02-14 10:12:04 +0300

    Regular Expressions. Quantifiers and the Pattern and Matcher classes

--- CJLRegularExpressions/src/as/IntroductionMain.java
+++ CJLRegularExpressions/src/as/IntroductionMain.java
@@ -13,13 +13,33 @@ Quantifiers
 */
 
 /*
+Boundaries matchers:
+
     ^ - the beginning of a pattern (but if it's in the [], [^...] is a character class, it's not a boundary matcher)
     $ - the end of a pattern
+
+    \\d - all digits
+    \\D - all non-digits
+    \\s - all whitespaces
+    \\S - all non-whitespaces
+    \\w - all letters and digits
+    \\b - all boundaries around the words
+    .   - every character
+    *   - zero or more character
+
+Quantifiers:
+
+    {} - quantities of a symbol (like "a{3}" that matches "aaa")
+    +  - one or more symbol (like "e+" that matches one or more "e")
+    *  -
 */
 
 /*
 IntellyJ IDEA hint: press Alt + Enter on a regex and choose Check RegExp to test it
 */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IntroductionMain {
     public static void main(String[] args) {
@@ -96,5 +116,47 @@ public class IntroductionMain {
         // output: XIX XhaveX XblanksX XandX	XaX XtabX, XandX XalsoX XaX XnewX XlineX
         // all boundaries of the words are replaced
         System.out.println(hasWhitespace.replaceAll("\\b", "X"));
+
+        // Quantifiers
+
+        String thirdAlphanumericString = "abcDeeeF12Ghhiiiijkl99z";
+        String thirdAlphanumericString1 = "abcDF12Ghhiiiijkl99z";
+        String thirdAlphanumericString2 = "abcDeF12Ghhiiiijkl99z";
+
+        // output:  YYYF12Ghhiiiijkl99z
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDeee", "YYY"));
+        // output:  YYYF12Ghhiiiijkl99z     symbol{number} - defines the quantity of the symbol in a regex
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe{3}", "YYY"));
+        // output:  YYYF12Ghhiiiijkl99z     +  - one or more "e"
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe+", "YYY"));
+        // output:  YYYF12Ghhiiiijkl99z
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe*", "YYY"));
+
+        // output:  abcDF12Ghhiiiijkl99z    no matches
+        System.out.println(thirdAlphanumericString1.replaceAll("^abcDe{3}", "YYY"));
+        // output:  abcDF12Ghhiiiijkl99z    no matches
+        System.out.println(thirdAlphanumericString1.replaceAll("^abcDe+", "YYY"));
+        // output:  YYYF12Ghhiiiijkl99z    matches because * means to match whether there is "e" or not
+        System.out.println(thirdAlphanumericString1.replaceAll("^abcDe*", "YYY"));
+
+        // output:  YYYF12Ghhiiiijkl99z
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe{2,5}", "YYY"));
+        // output:  abcDeF12Ghhiiiijkl99z   no matches (because we're looking between 2 and 5 "e")
+        System.out.println(thirdAlphanumericString2.replaceAll("^abcDe{2,5}", "YYY"));
+
+        // output: abcDeeeF12GYkl99z    all the "h, i, and j" symbols are replaces by "Y"
+        System.out.println(thirdAlphanumericString.replaceAll("h+i*j", "Y"));
+
+        StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
+        htmlText.append("<h2>Sub-heading</h2>");
+        htmlText.append("<p>This is a paragraph about something.</p>");
+        htmlText.append("<p>This is another paragraph about something else.</p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p>Here is the summary.</p>");
+
+        String h2pattern = ".*<h2>.*";
+        Pattern pattern = Pattern.compile(h2pattern, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+        Matcher matcher = pattern.matcher(htmlText);
+        System.out.println(matcher.matches()); // return true
     }
 }
